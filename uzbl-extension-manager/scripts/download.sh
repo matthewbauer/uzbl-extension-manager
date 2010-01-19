5ce3e4da04b2a73337201fec0c0734199149dbda
Matthew Bauer <mjbauer95@gmail.com>
2010-01-19 17:26:06 -0600

    Changed .uzbl.tar.gz to .uzbl

--- uzbl-extension-manager/scripts/download.sh
+++ uzbl-extension-manager/scripts/download.sh
@@ -14,7 +14,7 @@ export http_proxy
 test "x$url" = "x" && { echo "you must supply a url! ($url)"; exit 1; }
 
 # only changes the dir for the $get sub process
-if echo "$url" | grep -E '.*\.uzbl.tar.gz' >/dev/null;
+if echo "$url" | grep -E '^.*\.uzbl$' >/dev/null;
 then
 	( tmp=$(mktemp); $GET "$url" -O $tmp; $XDG_DATA_HOME/uzbl/extensions/uzbl-extension-manager/scripts/uzbl-install $tmp )
 else
